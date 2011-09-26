525d37921c339aaca4e0cccf6543dc2938098719
Jeremy Harris <jharris@rockharbor.org>
2011-09-26 13:45:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RH Central
+
+This is the WordPress child theme for RH Central. It inherits most of its
+templates and functionality from the __ROCK__HARBOR theme base.
+
+See the base theme for more information.
---
+++ libs/child_theme.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * RH Central theme
+ * 
+ * @package rockharbor
+ * @subpackage central
+ */
+class ChildTheme extends RockharborThemeBase {
+		
+/**
+ * List of options for this theme
+ * 
+ * @var array
+ */
+	protected $themeOptions = array(
+		'slug' => 'central',
+		'short_name' => 'Central'
+	);
+}
